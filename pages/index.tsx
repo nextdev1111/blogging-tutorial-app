e8ed3f12254484afa33e307440a1b0fe404a4373
NextDev <nextdevelopment1111@gmail.com>
2022-12-06 20:35:33 +0530

    Deployment

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,86 +1,61 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import type { GetStaticProps, NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import services from "@/services/index";
+import Container from "../layouts/Container";
+import LatestBlog from "components/blogs/LatestBlog";
+import Blogs from "components/blogs/Blogs";
+import Link from "next/link";
 
-const Home: NextPage = () => {
+/*
+
+List
+5. Sharer
+3. Deployment
+4. On Demand Revalidation
+
+ */
+
+type Props = {
+  blogs: Blog[];
+};
+
+const Home = ({ blogs }: Props) => {
+  console.log(blogs);
+
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <Container meta={{ title: "Next Dev" }}>
+      <div>
+        {/* latest blog */}
+        <LatestBlog blog={blogs[0]} />
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+        <hr className="w-full border-1 border-zinc-200 dark:border-zinc-800 mb-8 my-5" />
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
+        {/* blogs */}
+        <Blogs blogs={blogs.slice(1, 3)} />
+      </div>
 
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+      {/* show more */}
+      <div className="my-4">
+        <Link
+          href="/blogs"
+          className=" bg-zinc-200 dark:bg-zinc-800 px-3 py-2 rounded-md cursor-pointer"
         >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
-    </div>
-  )
-}
+          Show More
+        </Link>
+      </div>
+    </Container>
+  );
+};
 
-export default Home
+export default Home;
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const blogs = await services.queryBlogs();
+
+  return {
+    props: {
+      blogs,
+    },
+  };
+};
