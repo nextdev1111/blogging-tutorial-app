bcd678c462dbf89ac14d0bffb895d881f5db12db
NextDev <nextdevelopment1111@gmail.com>
2022-12-06 20:51:23 +0530

    Revalidation 1

---
+++ pages/api/revalidate.ts
@@ -0,0 +1,30 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+
+    // checking for the secret
+    if(req.headers['secret' ] !== process.env.SECRET){
+        return res.status(401).json({message: 'Invalid token'})
+    }
+
+    try {
+        
+        await res.revalidate('/')
+        await res.revalidate('/blogs')
+        await res.revalidate(`/blog/${req.body.data.content.slug}`)
+
+        return res.json({revalidated: true})
+
+    } catch (error) {
+        return res.status(500).send({
+            error: 'Error'
+        })
+    }
+
+}
