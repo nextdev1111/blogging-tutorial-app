9d6d9e73266e6f91930d73acf7c91a5319d10ee8
NextDev <nextdevelopment1111@gmail.com>
2022-12-07 10:59:50 +0530

    Revalidation Fixing

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -19,7 +19,7 @@ export default async function handler(
         await res.revalidate('/blogs')
         await res.revalidate(`/blog/${req.body.data.slug}`)
 
-        return res.json({revalidated: true, changeSlug: req.body.data})
+        return res.json({revalidated: true})
 
     } catch (error) {
         return res.status(500).send({
